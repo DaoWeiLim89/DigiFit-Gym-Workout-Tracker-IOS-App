ea8ebf86d9945ac8e13751b53887f19fc56b1724
Dao Wei Lim <109159208+DaoWeiLim89@users.noreply.github.com>
2025-10-31 20:36:09 -0400

    Initial Commit

---
+++ DigiFit: Gym Workout Tracker/DigiFit__Gym_Workout_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DigiFit__Gym_Workout_TrackerApp.swift
+//  DigiFit: Gym Workout Tracker
+//
+//  Created by Dao Wei Lim on 31/10/25.
+//
+
+import SwiftUI
+
+@main
+struct DigiFit__Gym_Workout_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DigiFit: Gym Workout TrackerTests/DigiFit__Gym_Workout_TrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  DigiFit__Gym_Workout_TrackerTests.swift
+//  DigiFit: Gym Workout TrackerTests
+//
+//  Created by Dao Wei Lim on 31/10/25.
+//
+
+import Testing
+@testable import DigiFit__Gym_Workout_Tracker
+
+struct DigiFit__Gym_Workout_TrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
